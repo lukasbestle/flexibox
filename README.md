08f7f80a81a4ac110a84b77e86e778fb50f2240b
Lukas Bestle <lukas@lu-x.me>
2014-03-09 12:39:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Flexibox
+
+> Simple lightbox without any complicated stuff around it
+
+## Why
+
+You all know these huge jQuery lightbox plugins, right?  
+Besides these 60kB (Fancybox) or even more, you also have to include jQuery in your site.
+
+I found out that I don't need jQuery in my daily life - it would make my life easier but I really don't like overhead.
+
+So I decided to write basic JS libraries not requiring any other libraries.  
+Also, I want to learn advanced JS, so building this without jQuery was good for me (see "Feedback" below).
+
+Flexibox is the second library in this series after I released [Flexisel](https://github.com/vis7mac/flexisel), a carousel library, two months ago.
+
+## Features
+
+- based on a set of lightboxable links and a "stage" attached to the body
+- displays the stage using a custom class name, which will get attached when the lightbox is active (you will do something like `opacity: 0;` combined with transitions and some "position outside of canvas" stuff for that class)
+- can be triggered manually by the user (clicking a link) or by the developer (`Flexibox.open()` and `Flexibox.close()` methods)
+- can be closed by clicking on the "background" of the overlay, but this behaviour can be fully customized
+- can exist multiple times on a page (different lightbox styles, yay!)
+
+## Flexisel is:
+
+- a simple lightbox framework
+- 797 bytes minified and gzipped (!)
+- totally flexible, as it doesn't require any other libraries
+
+## Flexisel is *not*:
+
+- packed with any CSS or graphics, so it can be highly configured (there is example CSS, but it isn't required!)
+- capable of displaying galleries or anything else than images
+- usable out-of-the-box™
+- working in stone-age browsers (requires `document.querySelector`)
+
+## Usage
+
+→ `example.html`
+
+## Feedback
+
+I'm truly not a JS professional - but I want to become one.
+
+I tried to comment everything in the Flexibox code, so if you want to support me and are good at JS, I would be really happy if you would give me [feedback on my code](https://twitter.com/lukasbestle).  
+**Thanks!**
---
+++ flexibox.js
@@ -0,0 +1,219 @@
+/**
+ * Flexibox
+ * Simple lightbox without any complicated stuff around it
+ * 
+ * @version   0.9 (09.03.2014)
+ * @author    Lukas Bestle <lukas@lu-x.me>
+ * @copyright Lukas Bestle
+ * @link      https://github.com/vis7mac/flexibox
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * 
+ * @param  object links   A list of all links the user can trigger to open the lightbox
+ * @param  object options Object with configuration options overwriting the default ones
+ */
+var Flexibox = function(links, options) {
+	'use strict';
+	
+	var self = this,
+	    attrName,
+	    stageObj,
+	    titleObj,
+	    imageObj,
+	    addClass,
+	    removeClass,
+	    addEvent;
+	
+	// Expose default options
+	this.options = {
+		// Selectors
+		stage:        '.lightboxstage', // Stage in body
+		title:        'span',           // Title box in stage
+		image:        'img',            // Image in stage
+		
+		// Class names
+		activeClass:  'active',         // Class attached to the stage while changing its content
+		
+		// Callbacks
+		openCallback:  null,            // Callback called with image, title and stage when opening the lightbox;
+		                                // can return false to prevent opening the lightbox
+		closeCallback: null,            // Callback called with the stage when closing the lightbox;
+		                                // can return false to prevent closing the lightbox
+		
+		// Other options
+		scrolling:    true,             // Disable scrolling on body while open?
+		debug:        false             // Debug mode (print to console each time the lightbox is opened or closed)
+	};
+	
+	// Merge with custom options
+	if(typeof options === 'object') {
+		for(attrName in options) {
+			if(options.hasOwnProperty(attrName)) {
+				this.options[attrName] = options[attrName];
+			}
+		}
+	}
+	
+	// Get required stuff from the DOM
+	stageObj = document.querySelector(this.options.stage);
+	titleObj = stageObj.querySelector(this.options.title);
+	imageObj = stageObj.querySelector(this.options.image);
+	links    = Array.prototype.slice.call(links);
+	
+	// Expose status stuff
+	window.flexibox = {};
+	this.active = window.flexibox.active = false; // Is the lightbox currently active?
+	this.data   = {                               // Information about the currently active item
+		'image': null,
+		'title': null
+	};
+	
+	/**
+	 * Opens a lightbox with a specified image and title
+	 * 
+	 * @param  string  image The image URL to load
+	 * @param  string  title A title to display in the lightbox
+	 * @return boolean       Did it work?
+	 */
+	this.open = function(image, title) {
+		// Don't open if already open
+		if(window.flexibox.active) {
+			if(this.options.debug) {
+				console.log('[Flexibox] Lightbox is already open!');
+			}
+			return false;
+		}
+		
+		// Ask the callback
+		if(this.options.openCallback && !this.options.openCallback(image, title, stageObj)) {
+			if(this.options.debug) {
+				console.log('[Flexibox] Callback aborted opening the lightbox with image "' + image + '" and title "' + title + '".');
+			}
+			return false;
+		}
+		
+		// Set status
+		this.active = window.flexibox.active = true;
+		this.data   = {
+			'image': image,
+			'title': title
+		};
+		
+		// Set image
+		imageObj.src   = image;
+		imageObj.alt   = title;
+		imageObj.title = title;
+		
+		// Set title
+		titleObj.innerHTML = title;
+		
+		// Disable scrolling on body
+		if(this.options.scrolling) {
+			document.body.style.overflow = 'hidden';
+		}
+		
+		// Show the stage
+		addClass(stageObj, this.options.activeClass);
+		
+		if(this.options.debug) {
+			console.log('[Flexibox] Opened lightbox with image "' + image + '" and title "' + title + '".');
+		}
+		return true;
+	};
+	
+	/**
+	 * Closes the lightbox
+	 * 
+	 * @param  object  event Optional: The click event which triggered closing the lightbox to pass to the callback
+	 * @return boolean       Did it work?
+	 */
+	this.close = function(event) {
+		// Don't close if not open
+		if(!window.flexibox.active) {
+			if(this.options.debug) {
+				console.log('[Flexibox] Lightbox is not open!');
+			}
+			return false;
+		}
+		
+		// Ask the callback
+		if(this.options.closeCallback && !this.options.closeCallback(stageObj, event)) {
+			if(this.options.debug) {
+				console.log('[Flexibox] Callback aborted closing the lightbox.');
+			}
+			return false;
+		}
+		
+		// Set status
+		this.active = window.flexibox.active = false;
+		this.data   = {
+			'image': null,
+			'title': null
+		};
+		
+		// Enable scrolling on body
+		if(this.options.scrolling) {
+			document.body.style.overflow = null;
+		}
+		
+		// Hide the stage
+		removeClass(stageObj, this.options.activeClass);
+		
+		if(this.options.debug) {
+			console.log('[Flexibox] Closed lightbox.');
+		}
+		return true;
+	};
+	
+	/**
+	 * Helper functions
+	 */
+	addClass = function(element, className) {
+		element.className += ((element.className)? ' ' : '') + className;
+	};
+
+	removeClass = function(element, className) {
+		element.className = element.className.replace(new RegExp('(\\s|^)' + className + '(\\s|$)'), ' ').replace(/^\s+|\s+$/g, '');
+	};
+	
+	addEvent = function(obj, type, fn) {
+		if(obj.addEventListener) {
+			obj.addEventListener(type, fn, false);
+		} else if (obj.attachEvent) {
+			obj['e' + type + fn] = fn;
+			obj[type + fn]       = function() {
+				obj['e' + type + fn](window.event);
+			};
+			
+			obj.attachEvent('on' + type, obj[type + fn]);
+		}
+	};
+	
+	/*
+	 * Click handlers
+	 */
+	addEvent(stageObj, 'click', function(event) {
+		// Check if it is our responsibility
+		if(!self.active) {
+			// No, skip to next event
+			return false;
+		}
+		
+		// Check if the background has been clicked
+		// If there is a callback, let it do that check
+		if(!self.options.closeCallback && event.target !== stageObj) {
+			return;
+		}
+		
+		self.close(event);
+	});
+	
+	links.forEach(function(link) {
+		// Add a click handler for this link
+		addEvent(link, 'click', function(event) {
+			self.open(link.href, link.title);
+			
+			// Prevent redirection
+			event.preventDefault();
+		});
+	});
+};
